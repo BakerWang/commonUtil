da12f75ef1fe819d5f2cc07f24555d3aa944ec23
皇族灬战狼 <duleilewuhen@163.com>
2016-04-27 16:49:53 +0800

    快递信息查询接口,返回String

--- src/main/java/com/cdeledu/webCrawler/CtripHelperUtil.java
+++ src/main/java/com/cdeledu/webCrawler/CtripHelperUtil.java
@@ -3,6 +3,8 @@ package com.cdeledu.webCrawler;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.cdeledu.datetime.DateUtilHelper;
 import com.cdeledu.network.tcp.HttpURLConnHelper;
 
@@ -15,6 +17,7 @@ import com.cdeledu.network.tcp.HttpURLConnHelper;
  */
 public class CtripHelperUtil {
 	/** ----------------------------------------------------- Fields start */
+	public static final String WULIU = "http://www.kuaidi100.com/query?type=%s&postid=%s";
 
 	/** ----------------------------------------------------- Fields end */
 
@@ -56,5 +59,30 @@ public class CtripHelperUtil {
 			}
 		}
 		return (null == sb) ? "" : sb.substring(0, sb.lastIndexOf("\n\n"));
+	}
+
+	/**
+	 * @方法描述: 快递接口
+	 * @创建者: 皇族灬战狼
+	 * @创建时间: 2016年4月27日 下午4:30:18
+	 * @param type
+	 *            快递公司代号
+	 *            <ul>
+	 *            <li>申通:shentong;EMS:ems;顺丰:shunfeng;圆通:yuantong;</li>
+	 *            <li>中通:zhongtong;韵达:yunda;天天:tiantian;</li>
+	 *            <li>汇通:huitongkuaidi;全峰:quanfengkuaidi</li>
+	 *            <li>德邦:debangwuliu;宅急送:zhaijisong</li>
+	 *            </ul>
+	 * @param postid
+	 *            快递单号
+	 * @return
+	 */
+	public static String getKuaidiInfo(String type, String postid) {
+		String result = "";
+		if (StringUtils.isNotEmpty(type) && StringUtils.isNotEmpty(postid)) {
+			String url = String.format(WULIU, type, postid);
+			result = HttpURLConnHelper.sendGetRequest(url);
+		}
+		return result;
 	}
 }
