9fa3d52c1b37945a20798bf32e62a5ddeb7712ad
皇族灬战狼 <duleilewuhen@163.com>
2016-04-29 17:41:27 +0800

    读取Tomcat的运行状态

--- src/main/java/com/cdeledu/application/system/ServerUtils.java
+++ src/main/java/com/cdeledu/application/system/ServerUtils.java
@@ -1,13 +1,21 @@
 package com.cdeledu.application.system;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 
 import com.cdeledu.application.commons.model.MemInfo;
+
+import sun.misc.BASE64Encoder;
 
 /**
  * @类描述: 服务器工具，用来读取服务器相关信息
@@ -77,5 +85,58 @@ public class ServerUtils {
 		int free = Integer.parseInt(details[2]);
 
 		return new MemInfo(total, used, free);
+	}
+
+	/**
+	 * @方法描述: 读取Tomcat的运行状态
+	 * @创建者: 皇族灬战狼
+	 * @创建时间: 2016年4月29日 下午3:22:51
+	 * @param serverHost
+	 *            tomcat 地址
+	 * @param serverPort
+	 *            tomcat 端口
+	 * @param userName
+	 *            管理用户名
+	 * @param passWord
+	 *            管理用户密码
+	 * @return
+	 */
+	public static String getTomcateRuntimeInfo(String serverHost, Integer serverPort, String userName, String passWord) {
+		String result = "";
+		URL url = null;
+		BufferedReader breader = null;
+		StringBuffer temp = new StringBuffer();
+		Document document = null;
+		
+		if(StringUtils.isEmpty(serverHost)){
+			return result;
+		}
+		if(null == serverPort || StringUtils.isEmpty(String.valueOf(serverPort))){
+			serverPort=80;
+		}
+		try {
+			String tomcatUrl = "http://%s:%s/manager/status?XML=true";
+			url = new URL(String.format(tomcatUrl, serverHost, serverPort));
+			String userPassword = userName + ":" + passWord;
+			String encoding = new BASE64Encoder().encode(userPassword.getBytes());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestProperty("Authorization", "Basic " + encoding);
+			breader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			while ((line = breader.readLine()) != null) {
+				temp.append(line);
+			}
+			// 将字符串转化为XML的Document
+			document = DocumentHelper.parseText(temp.toString());
+			result = document.asXML();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(breader);
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		getTomcateRuntimeInfo("localhost", 80, "admin", "admin");
 	}
 }
