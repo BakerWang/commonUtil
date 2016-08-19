4ae156c792965e185d8f8eeff7b505e8c40c4f21
皇族灬战狼 <duleilewuhen@163.com>
2016-08-19 20:43:08 +0800

    网络爬虫枚举类型

---
+++ src/main/java/com/cdeledu/webCrawler/type/CrawlType.java
@@ -0,0 +1,12 @@
+package com.cdeledu.webCrawler.type;
+
+/**
+ * @类描述: 网络爬虫枚举类型
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2016年8月17日 下午9:10:03
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public enum CrawlType {
+	jsoup, urlconn, httpclient, htmlunit, htmlparse, selenium;
+}
