c1f4e1b3de4001472f07eb318a28b98fe38f21d7
独泪了无痕 <duleilewuhen@sina.com>
2016-09-11 19:57:59 +0800

    爬虫元素

---
+++ src/main/java/com/cdeledu/webCrawler/crawler/util/CrawlerUrl.java
@@ -0,0 +1,21 @@
+package com.cdeledu.webCrawler.crawler.util;
+
+import com.cdeledu.webCrawler.crawler.type.SchemType;
+
+/**
+ * @类描述: TODO(这里用一句话描述这个类的作用)
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建日期: 2016年9月10日 上午11:05:19
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="">TODO(连接内容简介)</a>
+ */
+public class CrawlerUrl {
+	/** ----------------------------------------------------- Fields start */
+	public String sUrl; // URL字符串
+	public SchemType scheme; // URL字符串
+	public String sHost; // 主机名
+	public String sPort; // 端口号
+	public String sPath; // 请求资源
+	/** ----------------------------------------------------- Fields end */
+}
