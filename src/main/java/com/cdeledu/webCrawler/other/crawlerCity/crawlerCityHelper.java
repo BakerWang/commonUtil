aba5970b57fbc10dd5a6571b628fa5c30ef8f364
皇族灬战狼 <duleilewuhen@163.com>
2016-11-21 15:02:24 +0800

    原爬虫整体爬取现改为分级爬取，这样尽可能避免出现异常！

--- src/main/java/com/cdeledu/webCrawler/other/crawlerCity/crawlerCityHelper.java
+++ src/main/java/com/cdeledu/webCrawler/other/crawlerCity/crawlerCityHelper.java
@@ -1,11 +1,17 @@
 package com.cdeledu.webCrawler.other.crawlerCity;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,17 +20,17 @@ import org.jsoup.select.Elements;
 import com.cdeledu.database.DataTableHelper;
 import com.cdeledu.network.common.util.IpUtilHelper;
 import com.cdeledu.webCrawler.crawler.type.UserAgentType;
-import com.cdeledu.webCrawler.crawler.util.WebCrawlerHelper;
 
 /**
  * @类描述: 利用网络爬虫端抓取行政区域数据
  * @创建者: 皇族灬战狼
  * @创建时间: 2016年8月16日 下午8:08:57
- * @版本: V1.0
+ * @版本: V1.2.1
  * @since: JDK 1.7
  */
 public class crawlerCityHelper {
 	/** ----------------------------------------------------- Fields start */
+	private static Logger logger = Logger.getLogger(crawlerCityHelper.class);
 	private static ResourceBundle JDBC = ResourceBundle.getBundle("datasource/jdbc");
 	private static String dbUrl = JDBC.getString("database.dbUrl");
 	private static String dbUserName = JDBC.getString("database.dbUserName");
@@ -32,21 +38,178 @@ public class crawlerCityHelper {
 	private static String jdbcName = JDBC.getString("database.jdbcName");
 	/** SQL执行工具 :实例化查询接口 */
 	private static QueryRunner runner = null;
-	private static DataTableHelper dataSource = null;
+	private static DataTableHelper dataTableHelper = null;
 	/** SQL执行工具:执行SQL语句 */
-	private static String inserSql = "INSERT INTO sys_date_cityInfo(proName,proCode,parentId,proLevel,proType) VALUES ('%s', '%s', %s, '%s','%s')";
+	private static String SQL = "INSERT INTO sys_date_cityInfo"
+			+ "(areaName,areaCode,areaUrl,parentId,areaLevel,areaType)"
+			+ " VALUES ('%s', '%s','%s', %s, '%s','%s')";
+	private static String ISEXIST = "SELECT COUNT(1) FROM sys_date_cityinfo WHERE areaCode = '%s'";
 	/** ----------------------------------------------------- Fields end */
 
 	static {
-		dataSource = DataTableHelper.getInstance(dbUrl, dbUserName, dbPassword, jdbcName);
+		dataTableHelper = DataTableHelper.getInstance(dbUrl, dbUserName, dbPassword, jdbcName);
 		try {
-			runner = dataSource.getQueryRunner();
+			runner = dataTableHelper.getQueryRunner();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/** ----------------------------------------------- [私有方法] */
+
+	/**
+	 * @方法描述: 获取省份代码
+	 * @return
+	 */
+	private static String getProvinceCode(String val) {
+		if (val.indexOf(".") == -1)
+			return val;
+		return (String) val.substring(0, val.indexOf("."));
+	}
+
+	/**
+	 * @方法描述: 查询是否已经该条数据
+	 * @param val
+	 * @return
+	 */
+	private static boolean isExist(String val) {
+		int resultNum = 0;
+		if (StringUtils.isNotBlank(val)) {
+			resultNum = dataTableHelper.getCount(runner, String.format(ISEXIST, val));
+		}
+		return resultNum > 0 ? true : false;
+	}
+
+	/**
+	 * 
+	 * @方法描述: 将得到的document进行解析 存入数据库
+	 * @param sql
+	 *            执行sql
+	 * @throws SQLException
+	 */
+	private static void saveDocument(String inserSql) throws SQLException {
+		if (logger.isDebugEnabled()) {
+			logger.info("解析数据并保存入数据库:" + inserSql);
+		}
+		runner.insert(inserSql, new ScalarHandler<Long>()).intValue();
+	}
+
+	/**
+	 * @方法描述: 地级行政区划单位(地级市、地区、自治州、盟)
+	 * @param parentId
+	 * @param crawlerUrl
+	 */
+	private static void saveCityInfo(int parentId, String crawlerUrl) {
+		Document document = getDocument(crawlerUrl);
+		Elements eles01 = document.getElementsByAttributeValue("class", "citytr");
+		for (Element ele01 : eles01) {
+			Elements eles02 = ele01.getElementsByTag("a");
+			if (eles02.toString().trim().equals("")) {// 表示没有a标签
+				continue;
+			}
+			String code = eles02.get(0).text(); // 城市代码
+			String name = eles02.get(1).text();// 城市名称
+			String url = eles02.get(0).absUrl("href");
+			String inserSql = String.format(SQL, name, code, url, parentId, 2, "");
+			if (!isExist(code)) { // 查找是否已存在,不存在则插入
+				try {
+					saveDocument(inserSql);
+				} catch (Exception e) {
+					logger.error("获取地级行政区数据之时出现异常:", e);
+					saveCityInfo(parentId, crawlerUrl);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @方法描述: 县级行政区划单位(市辖区、县级市、县、自治县、旗、自治旗、特区、林区)
+	 * @param runner
+	 * @param url
+	 * @return
+	 */
+	private static void saveCountyInfo(int parentId, String crawlerUrl) {
+		Document document = getDocument(crawlerUrl);
+
+		Elements eles01 = document.getElementsByAttributeValue("class", "countytr");
+		for (Element ele01 : eles01) {
+			Elements eles02 = ele01.getElementsByTag("a");
+			if (eles02.toString().trim().equals("")) {// 表示没有a标签
+				continue;
+			}
+			String code = eles02.get(0).text();// 县级代码
+			String name = eles02.get(1).text();// 县级名称
+			String url = eles02.get(0).absUrl("href");
+
+			String inserSql = String.format(SQL, name, code, url, parentId, 3, "");
+			if (!isExist(code)) { // 查找是否已存在,不存在则插入
+				try {
+					saveDocument(inserSql);
+				} catch (Exception e) {
+					logger.error("获取县级行政区数据之时出现异常:", e);
+					saveCountyInfo(parentId, crawlerUrl);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @方法描述: 乡级行政区划单位(区公所、街道、镇、乡、民族乡、苏木、民族苏木)
+	 * @param parentId
+	 * @param crawlerUrl
+	 */
+	private static void saveTownInfo(int parentId, String crawlerUrl) {
+		Document document = getDocument(crawlerUrl);
+		Elements eles01 = document.getElementsByAttributeValue("class", "towntr");
+		for (Element ele01 : eles01) {
+			Elements eles02 = ele01.getElementsByTag("a");
+			if (eles01.toString().trim().equals("")) {// 表示没有a标签
+				continue;
+			}
+			String code = eles02.get(0).text();// 乡级行政区代码
+			String name = eles02.get(1).text();// 乡级行政区名称
+			String url = eles02.get(1).absUrl("href");
+			String inserSql = String.format(SQL, name, code, url, parentId, 4, "");
+			if (!isExist(code)) { // 查找是否已存在,不存在则插入
+				try {
+					saveDocument(inserSql);
+				} catch (Exception e) {
+					logger.error("获取乡级行政区数据之时出现异常:", e);
+					saveTownInfo(parentId, crawlerUrl);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @方法描述: 村级行政单位(村民委员会、居民委员会、类似村民委员会、类似居民委员)
+	 * @param parentId
+	 * @param crawlerUrl
+	 */
+	private static void saveVillageInfo(int parentId, String crawlerUrl) {
+		Document document = getDocument(crawlerUrl);
+		Elements eles01 = document.getElementsByAttributeValue("class", "villagetr");
+		for (Element ele01 : eles01) {
+			Elements eles02 = ele01.getElementsByTag("td");
+			// 乡级行政区代码
+			String code = eles02.get(0).text();
+			// 乡级行政区名称
+			String name = eles02.get(2).text();
+			// 城乡分类代码
+			String type = eles02.get(1).text();
+			String inserSql = String.format(SQL, name, code, null, parentId, 5, type);
+			if (!isExist(code)) { // 查找是否已存在,不存在则插入
+				try {
+					saveDocument(inserSql);
+				} catch (Exception e) {
+					saveVillageInfo(parentId, crawlerUrl);
+				}
+			}
+		}
+	}
+
+	/** ----------------------------------------------- [私有方法] */
+
 	/**
 	 * @方法描述: 通过地址得到document对象
 	 * @创建者: 皇族灬战狼
@@ -58,10 +221,12 @@ public class crawlerCityHelper {
 		Connection conn = null;
 		Document document = null;
 		try {
-			conn = Jsoup.connect(url);
-			conn.header("User-Agent", UserAgentType.PC_Firefox.getValue());
+			conn = Jsoup.connect(url);//获取连接
+			conn.header("Connection", "keep-alive");
+			conn.header("User-Agent", UserAgentType.PC_Firefox.getValue());//配置模拟浏览器
+			conn.method(Method.POST);
 			conn.timeout(10000);
-			document = conn.timeout(10000).get();
+			document = conn.execute().parse();//获取响应
 			// 表示ip被拦截或者其他情况,反正没有获取document对象
 			if (null == document || "".equals(document.toString())) {
 				IpUtilHelper.setProxyIp(); // 换代理ip
@@ -77,159 +242,106 @@ public class crawlerCityHelper {
 	}
 
 	/**
-	 * @方法描述: 获取省份代码
-	 * @return
+	 * @方法描述: 省级行政区(省、自治区、直辖市、特别行政区)
+	 * @throws Exception
 	 */
-	private static String getProvinceCode(String val) {
-		if (val.indexOf(".") == -1)
-			return val;
-		return (String) val.substring(0, val.indexOf("."));
-	}
-
-	/**
-	 * @方法描述: 将得到的document进行解析 存入数据库
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2016年8月17日 下午5:39:21
-	 * @param document
-	 */
-
-	private static void analysisDocument(Document document) throws Exception {
-		Elements eles1 = document.getElementsByAttributeValue("class", "provincetr");
-		for (Element ele1 : eles1) {
-			Elements eles2 = ele1.getElementsByTag("a");
-			String proSql = "";
-			/**
-			 * 省级行政区(省、自治区、直辖市、特别行政区)
-			 */
-			for (Element ele2 : eles2) {
-				Document doc2 = getDocument(ele2.absUrl("href"));
-				int key1 = 0;
-				String provinceCode = getProvinceCode(ele2.attr("href"));
-				String provinceName = ele2.text();
-
-				proSql = String.format(inserSql, provinceName, provinceCode, "0", 1, "");
-				key1 = runner.insert(proSql, new ScalarHandler<Long>()).intValue();
-
-				/**
-				 * 地级行政区划单位(地级市、地区、自治州、盟)
-				 */
-
-				Elements eles3 = doc2.getElementsByAttributeValue("class", "citytr");
-				for (Element ele3 : eles3) {
-					Elements eles4 = ele3.getElementsByTag("a");
-					String citySql = "";
-					// 城市代码
-					String cityCode = "";
-					// 城市名称
-					String cityName = "";
-					int key2 = 0;
-
-					if (eles4.toString().trim().equals("")) {// 表示没有a标签
-						// Elements diffElements = ele3.getElementsByTag("td");
-						// cityCode = diffElements.get(0).text();
-						// cityName = diffElements.get(1).text();
+	public static void getProvinceInfo() {
+		/** 初始解析网页地址 */
+		String crawlerBaseUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2015/index.html";
+		Document document = getDocument(crawlerBaseUrl);
+		Elements eles01 = document.getElementsByAttributeValue("class", "provincetr");
+		for (Element ele01 : eles01) {
+			for (Element ele02 : ele01.getElementsByTag("a")) {
+				String code = getProvinceCode(ele02.attr("href"));// 省份代码
+				String name = ele02.text();// 省份名称
+				String url = ele02.absUrl("href");// 省份地址
+				String inserSql = String.format(SQL, name, code, url, "51240", 1, "");
+				if (!isExist(code)) { // 查找是否已存在,不存在则插入
+					try {
+						saveDocument(inserSql);
+					} catch (Exception e) {
+						logger.error("获取省级行政区数据之时出现异常:", e);
 						continue;
-					}
-
-					cityCode = WebCrawlerHelper.removeTheEnd(eles4.get(0).text(), "0");
-					cityName = eles4.get(1).text();
-					citySql = String.format(inserSql, cityName, cityCode, key1, 2, "");
-
-					key2 = runner.insert(citySql, new ScalarHandler<Long>()).intValue();
-
-					/**
-					 * 县级行政区划单位(市辖区、县级市、县、自治县、旗、自治旗、特区、林区)
-					 */
-
-					Document doc3 = getDocument(eles4.get(1).absUrl("href"));
-					Elements eles5 = doc3.getElementsByAttributeValue("class", "countytr");
-					for (Element ele5 : eles5) {
-						Elements eles6 = ele5.getElementsByTag("a");
-						String countySql = "";
-						String countyCode = "";// 县级代码
-						String countyName = "";// 县级名称
-						int key3 = 0;
-
-						if (eles6.toString().trim().equals("")) {// 表示没有a标签
-							// Elements diffElements =
-							// ele5.getElementsByTag("td");
-							// countyCode = diffElements.get(0).text();
-							// countyName = diffElements.get(1).text();
-							continue;
-						}
-						countyCode = WebCrawlerHelper.removeTheEnd(eles6.get(0).text(), "0");
-						countyName = eles6.get(1).text();
-
-						countySql = String.format(inserSql, countyName, countyCode, key2, 3, "");
-						key3 = runner.insert(countySql, new ScalarHandler<Long>()).intValue();
-
-						/**
-						 * 乡级行政区划单位(区公所、街道、镇、乡、民族乡、苏木、民族苏木)
-						 */
-
-						Document doc4 = getDocument(eles6.get(1).absUrl("href"));
-						Elements eles7 = doc4.getElementsByAttributeValue("class", "towntr");
-						for (Element ele7 : eles7) {
-							Elements eles8 = ele7.getElementsByTag("a");
-							String townSql = "";
-							String townCode = "";// 乡级行政区代码
-							String townName = "";// 乡级行政区名称
-							int key4 = 0;
-							if (eles8.toString().trim().equals("")) {// 表示没有a标签
-								// Elements diffElements =
-								// ele7.getElementsByTag("td");
-								// diffElements.get(0).text();
-								// diffElements.get(1).text();
-								continue;
-							}
-							townCode = WebCrawlerHelper.removeTheEnd(eles8.get(0).text(), "0");
-							townSql = String.format(inserSql, townName, townCode, key3, 4, "");
-							key4 = runner.insert(townSql, new ScalarHandler<Long>()).intValue();
-
-							/**
-							 * 村级行政单位(村民委员会、居民委员会、类似村民委员会、类似居民委员)
-							 */
-
-							Document doc5 = getDocument(eles8.get(1).absUrl("href"));
-							Elements eles9 = doc5.getElementsByAttributeValue("class", "villagetr");
-							for (Element ele9 : eles9) {
-								Elements eles10 = ele9.getElementsByTag("td");
-								String villageSql = "";
-								// 乡级行政区代码
-								String villageCode = eles10.get(0).text();
-								// 乡级行政区名称
-								String villageName = eles10.get(2).text();
-								// 城乡分类代码
-								String villageType = eles10.get(1).text();
-								villageSql = String.format(inserSql, villageName, villageCode, key4,
-										5, villageType);
-								runner.insert(villageSql, new ScalarHandler<Long>()).intValue();
-							}
-						}
 					}
 				}
 			}
 		}
 	}
 
-	/** ----------------------------------------------- [私有方法] */
 	/**
-	 * @方法描述: 利用网络爬虫端抓取数据
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2016年8月17日 下午9:06:22
+	 * @方法描述: 地级行政区划单位(地级市、地区、自治州、盟)
+	 * @return
 	 */
-	public static void webCrawler() throws Exception {
-		// 初始解析网页地址
-		String crawlerUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2015/index.html";
-		Document document = getDocument(crawlerUrl);// 得到的document一定是正常的
-		analysisDocument(document);
-	}
-
-	public static void main(String[] args) {
+	public static void getCityInfo() {
+		String sql = "SELECT id,areaUrl FROM sys_date_cityinfo WHERE areaLevel = 1";
+		List<Map<String, Object>> result = null;
 		try {
-			webCrawler();
+			result = runner.query(sql, new MapListHandler());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		for (Map<String, Object> object : result) {
+			saveCityInfo((Integer) object.get("id"), (String) object.get("areaUrl"));
+		}
+	}
+
+	/**
+	 * @方法描述: 县级行政区划单位(市辖区、县级市、县、自治县、旗、自治旗、特区、林区)
+	 * @param runner
+	 * @param url
+	 * @return
+	 */
+	public static void getCountyInfo() {
+		String sql = "SELECT id,areaUrl FROM sys_date_cityinfo WHERE areaLevel = 2";
+		List<Map<String, Object>> result = null;
+		try {
+			result = runner.query(sql, new MapListHandler());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (Map<String, Object> object : result) {
+			saveCountyInfo((Integer) object.get("id"), (String) object.get("areaUrl"));
+		}
+	}
+
+	/**
+	 * @方法描述: 乡级行政区划单位(区公所、街道、镇、乡、民族乡、苏木、民族苏木)
+	 * @return
+	 */
+	public static void getTownInfo() {
+		String sql = "SELECT id,areaUrl FROM sys_date_cityinfo WHERE areaLevel = 3";
+		List<Map<String, Object>> result = null;
+		try {
+			result = runner.query(sql, new MapListHandler());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (Map<String, Object> object : result) {
+			saveTownInfo((Integer) object.get("id"), (String) object.get("areaUrl"));
+		}
+	}
+
+	/**
+	 * @方法描述: 村级行政单位(村民委员会、居民委员会、类似村民委员会、类似居民委员)
+	 */
+	public static void getVillageInfo() {
+		String sql = "SELECT id,areaUrl FROM sys_date_cityinfo WHERE areaLevel = 4";
+		List<Map<String, Object>> result = null;
+		try {
+			result = runner.query(sql, new MapListHandler());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (Map<String, Object> object : result) {
+			saveVillageInfo((Integer) object.get("id"), (String) object.get("areaUrl"));
+		}
+	}
+
+	public static void main(String[] args) {
+		getProvinceInfo();
+		// getCityInfo();
+		// getCountyInfo();
+		// getTownInfo();
+		// getVillageInfo();
 	}
 }
